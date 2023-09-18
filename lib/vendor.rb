2aba012ed879a676c723d5721b83a88c0d56cf65
Scott DeVoss <scottdevoss@yahoo.com>
2023-09-18 09:30:03 -0600

    added tests for iteration 1

---
+++ lib/vendor.rb
@@ -0,0 +1,16 @@
+class Vendor
+  attr_reader :name, :inventory, :stock
+  def initialize(name)
+    @name = name
+    @inventory = {}
+    @stock = 0
+  end
+
+  def check_stock(item)
+    @stock
+  end
+
+  def stock(item, inventory)
+
+  end
+end
---
+++ spec/vendor_spec.rb
@@ -0,0 +1,37 @@
+require './lib/vendor'
+require './lib/item'
+
+RSpec.describe Vendor do
+  before(:each) do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+  describe '#initialize' do
+    it 'exists' do
+      expect(@vendor).to be_an_instance_of(Vendor)
+      expect(@vendor.name).to eq("Rocky Mountain Fresh")
+      expect(@vendor.inventory).to eq({})
+    end
+  end
+
+  describe '#check_stock(item)' do
+    it 'can check stock of an item' do
+      expect(@vendor.check_stock(@item1)).to eq(0)
+      @vendor.stock(@item1, 30)
+      expect(@vendor.check_stock(@item1)).to eq(30)
+    end
+  end
+
+  describe '#stock(item, inventory)' do
+    it 'can stock an item' do
+      expect(@vendor.inventory).to eq({})
+      @vendor.stock(@item1, 30)
+      expect(@vendor.inventory).to eq({@item1.name => 30})
+      @vendor.stock(@item1, 25)
+      expect(@vendor.inventory).to eq({@item1.name => 55})
+      @vendor.stock(@item2, 12)
+      expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
+    end
+  end
+end
